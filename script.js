dc8ff21f81f872373781a7692f3e764fcfa8e911
phoenixmessenger <42844921+phoenixmessenger@users.noreply.github.com>
2021-09-06 19:11:29 +0300

    Full CSS plus scripts for menu&modal (v. 1.26 from 06/09/2021)

--- script.js
+++ script.js
@@ -4,11 +4,57 @@ const adoptMenu = () => {
 	});
 }
 
+const adoptMenuOverLay = () => {
+	$(window).on('click', function(){
+		$('.burger_overlay').toggle();
+	});
+}
+
 const closWin = () => {
 	let screenwidth = $(window).width();
 	$(window).resize(function() {
-		if (screenwidth <= 600) {
+		if (screenwidth <= 1050) {
 			$('.mob-nav-menu').hide();
 		}
 	}).resize();
 }
+
+const modalOpenByClickToButton = () => {
+	$('.button').on('click', function() {
+		$('.modal').show();
+	})
+}
+
+const modalClose = () => {
+	let mClose = document.getElementByClassName('.modal_close');
+	mClose.onclick = function() {
+		$('.modal').hide();
+	}
+}
+
+// const modalOpenByScroll = () => {
+// 	let scrollheight = window.pageYOffset();
+// 	window.addEventListener('scroll', function() {
+// 		if(scrollheight === 1000) {
+// 			modal.style.display="block";
+// 		}
+// 	})
+// }
+
+
+// const modalCloseIcon = document.querySelectorAll('.close-modal');
+// 	if(modalCloseIcon.length > 0) {
+// 		for(let i = 0; i < modalCloseIcon.length; i++) {
+// 			const el = modalCloseIcon[i];
+// 			el.addEventListener('click' function(e) {
+// 				modalClose(el.closest('.modal'));
+// 				e.preventDefault();
+// 			})
+// 		}
+// 	}
+
+adoptMenu();
+adoptMenuOverLay();
+closWin();
+modalOpenByClickToButton();
+modalClose();
